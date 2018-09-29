136fa4139f9783328c6a4e47d7432439096e81cd
Yauheni Tsiarokhin <hihihiko00@gmail.com>
2018-09-29 13:49:45 +0300

    implement proper command line parsing
        - integrate npm
        - create .gitignore

--- main.js
+++ main.js
@@ -3,13 +3,21 @@
 const exec = require('child_process').exec;
 const fs = require('fs');
 const path = require('path');
+const argParser = require('command-line-args');
 const { rename } = require('./worker');
 const status = require('./status');
 
-const args = process.argv.slice(2);
+const optionsDefinition = [
+  { name: 'mode', alias: 'm', type: String },
+  { name: 'from', alias: 'f', type: String },
+  { name: 'to', alias: 't', type: String }
+];
+
+const { mode, from, to } = argParser(optionsDefinition);
 
 const root = process.cwd();
-const [input, output] = args.map(x => path.join(root, x));
+const input = path.join(root, from);
+const output = path.join(root, to);
 
 const processFiles = (files, cur, max) => {
   if (!files.length) {
--- worker.js
+++ worker.js
@@ -1,12 +1,15 @@
 const status = require('./status');
 
+const prepare = (str) => {
+    return str.replace(/(\[|\]|\(|\))/g,'').replace(/[ _]+/g, ' ');
+}
+
 const rename = (str, cur, max) => {
   status.process(str, cur, max);
 
-  const [artist, remainder] = str.toLowerCase().split(/ +- +/);
-  const title = remainder
-    .replace(/(\[|\]|\(|\))/g,'')
-    .replace(/[ _]+/g, ' ')
+  const [artist, rawTitle] = str.toLowerCase().split(/ +- +/);
+  const preparedTitle = prepare(rawTitle);
+  const title = preparedTitle
     .replace(/^([a-zA-Z0-9$ ]+?)( (feat|ft)\.? ?([a-zA-Z0-9$ ]+?( x [a-zA-Z0-9$ ]+?)*))?( (prod\.? ?by|prod)\.? ?([a-zA-Z0-9$ ]+?( x [a-zA-Z0-9$ ]+?)*))? *?(.mp3)$/,
       (...x) => {
         if (x[4] && x[8]) return `${x[1]} (w/ ${x[4]}) (p/ ${x[8]})`;
